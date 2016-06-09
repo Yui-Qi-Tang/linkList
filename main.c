bdc6f801f43505723f4741e978956571b787ba36
Yui-Qi Tang <yqtang@phi.yqHome.org>
2016-06-10 00:26:00 +0800

    link list in c version0.1-check list and free list

--- main.c
+++ main.c
@@ -1,60 +1,13 @@
-#include "stdio.h"
-#include "stdlib.h"
-
-typedef struct NODE
-{
-    int value;
-				struct NODE *next;
-}node;
-
-
-void dump(node* head)
-{
-    node *iterator = head;
-    while(iterator != NULL)
-				{
-				    printf("%d\n",iterator->value);
-								iterator = iterator->next;
-				}
-				printf("NULL");
-}
-
-/*Add new Node to tail of list */
-void addNode(node* listHead, int valueOfNewNode)
-{
-    /*Create new node instance*/
-    node *newNode = malloc(sizeof(node));
-				/*Set new value for new node from external*/
-				newNode->value=valueOfNewNode;
-
-    /*Pointer to head of list*/
-				node *pCurt = listHead; // Curt = current
-				/*Pointer to next of head of list*/
-				node *pCurtNext = listHead->next;
-    /*Move pCurt and pCurtNext until pNext is NULL*/
-				while(pCurtNext != NULL)
-				{
-				    pCurt = pCurtNext;
-								pCurtNext = pCurtNext->next;
-				}
-				/*next of new node is pNext, because pNext point to NULL now.*/
-				newNode->next = pCurtNext;
-				/*Add new node for pHead->next*/
-    pCurt->next = newNode;
-
-}
+#include "linkList.h"
 
 void main()
 {   
-				node *list = malloc(sizeof(node));
-				list -> value = 0;
-				list -> next = NULL;
-
-
+				node* list = createHead();
+    printf("Add 99, 100 and 101 to list\n");
 				addNode(list, 99);
 				addNode(list, 100);
 				addNode(list, 101);
-    
-				dump(list);
-				    
-}
+				dump(list); 
+				freeList(list);
+				
+}// End of main   
---
+++ src/linkList.c
@@ -0,0 +1,152 @@
+/**
+ *  @file       linkList.c
+ *  @brief      Defines Application 
+ *  @author     Yui Qi Tang (yqtang1222@gmail.com)
+ *  @date       06/09/2016 created.
+ *  @date       06/09/2016 last modified.
+ *  @version    0.01
+ *  @section    License
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation; either version 2 of
+ *  the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details at
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ *  @section DESCRIPTION
+ *
+ *  Link list functions
+ **/
+
+
+#include "linkList.h"
+
+/**
+ *  create head for list
+ *
+ *  @param          none.
+ *  @return         node*
+ *  @see
+ *  @since          0.01
+ **/
+node* createHead(){
+    node *head = malloc(sizeof(node));
+				head -> value = 0;
+				head -> next = NULL;
+				return head;    
+}// End of createHead()
+
+/**
+ *  Print value of each node of specify list
+ *
+ *  @param          node*
+ *  @return         none.
+ *  @see
+ *  @since          0.01
+ **/
+
+void dump(node* head)
+{
+    node *iterator = head->next;
+				printf("Head -> ");
+    while(iterator != NULL)
+				{
+				    printf("%d -> ",iterator->value);
+								iterator = iterator->next;
+				}
+				printf("Tail\n");
+}// End of dump()
+
+
+
+/**
+ *  free list
+ *
+ *  @param          node*
+ *  @return         none.
+ *  @see
+ *  @since          0.01
+ **/
+void freeList(node *list)
+{
+    node *tmp;
+
+				while(list != NULL)
+				{
+				    tmp = list;
+				    list = list -> next;
+				    free(tmp);				
+				}
+}// End of freeList()
+
+
+
+/**
+ *  Add new Node to specify list 
+ *
+ *  @param          node*, int
+ *  @return         none.
+ *  @see
+ *  @since          0.01
+ **/
+void addNode(node* listHead, int valueOfNewNode)
+{
+    /*Create new node instance*/
+    node *newNode = malloc(sizeof(node));
+				/*Set new value for new node from external*/
+				newNode->value=valueOfNewNode;
+
+    /*Pointer to head of list*/
+				node *pCurt = listHead; // Curt = current
+				/*Pointer to next of head of list*/
+				node *pCurtNext = listHead->next;
+    /*Move pCurt and pCurtNext until pNext is NULL*/
+				while(pCurtNext != NULL)
+				{
+				    pCurt = pCurtNext;
+								pCurtNext = pCurtNext->next;
+				}
+				/*next of new node is pNext, because pNext point to NULL now.*/
+				newNode->next = pCurtNext;
+				/*Add new node for pHead->next*/
+    pCurt->next = newNode;
+
+}// End of addNode
+
+/**
+ *  Check whether the specify valus is exist specify list or not.
+	*  Note : Because there is no bool data type in C,
+ *         so I use unsigned char to present bool
+ *  
+ *  @param          node*, int
+ *  @return         unsigned char.
+ *  @see
+ *  @since          0.01
+ **/
+unsigned char isExist(node *list,int target)
+{
+    //Init. pointer at head of list
+				node* head = list;
+				
+				while(head -> next != NULL)
+				{
+								//Check whether the target is exist or not
+								//if exist return true
+								//otherwise go next node of list
+				    if(head -> value == target)
+								{
+								    return true;
+								}
+								//next node of list
+								head = head->next;
+				}//fi
+				//not exist
+				return false;
+}// End of isExist
+
+
